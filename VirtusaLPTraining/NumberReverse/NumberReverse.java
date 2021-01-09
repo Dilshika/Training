a44ab1740a6abd9b7721f82f0dcf6151cc4872b3
Dilshika <dilshika.bandara@gmail.com>
2021-01-09 11:35:17 +0530

    Number Reverse

--- VirtusaLPTraining/NumberReverse/NumberReverse.java
+++ VirtusaLPTraining/NumberReverse/NumberReverse.java
@@ -1,33 +1,24 @@
 import java.util.Scanner;
+import java.lang.Long;
+
 
 public class NumberReverse{
 	public static void main(String []args){
+
+	long reverse=0;
+
 	Scanner scanner=new Scanner(System.in);
 	System.out.println("Enter any number");
 	long number=scanner.nextLong();
 
-	System.out.println("Reversed Number:");
-
-	if(number==0){
-	System.out.print(number);	//Input is zero
-	}
-
-	if(number<0){		//Number is negative
+	if(number> Long.MIN_VALUE || number<Long.MAX_VALUE){	 //Check the range
 	while(number!=0){
-
-	long remainder=number%10;  	//To achieve the last digit of the number
-	System.out.print(remainder);
-	number=-number/10;		//Remove the printed digit
-
-	}
-	}
-	while(number!=0){
-
-	long remainder=number%10;  	//To achieve the last digit of the number
-	System.out.print(remainder);
+	
+	reverse=reverse*10;	 	//To achieve the reversed number
+	reverse=reverse+number%10;  	//To achieve the last digit of the number and add to the reversed number
 	number=number/10;		//Remove the printed digit
-
 	}
-		
+	System.out.print("Reversed Number:"+reverse);
+	}	
 	}
 }
---
+++ VirtusaLPTraining/NumberReverse/README.txt
@@ -0,0 +1,3 @@
+Use: This code is used to reversed any given number in the long range. (-9 223 372 036 854 775 808 to +9 223 372 036 854 775 807/ 64 bits).
+
+Exception Error:The exeception error InputMismatchException if the next token cannot be translated into Long. In other words input exceed the long value range.
