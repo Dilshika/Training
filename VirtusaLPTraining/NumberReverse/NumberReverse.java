6c2947df74955535938ebd642394d2582eb3cbad
Dilshika <dilshika.bandara@gmail.com>
2021-01-09 09:54:26 +0530

    Number Reverse

--- VirtusaLPTraining/NumberReverse/NumberReverse.java
+++ VirtusaLPTraining/NumberReverse/NumberReverse.java
@@ -7,8 +7,20 @@ public class NumberReverse{
 	long number=scanner.nextLong();
 
 	System.out.println("Reversed Number:");
-	
-	
+
+	if(number==0){
+	System.out.print(number);	//Input is zero
+	}
+
+	if(number<0){		//Number is negative
+	while(number!=0){
+
+	long remainder=number%10;  	//To achieve the last digit of the number
+	System.out.print(remainder);
+	number=-number/10;		//Remove the printed digit
+
+	}
+	}
 	while(number!=0){
 
 	long remainder=number%10;  	//To achieve the last digit of the number
