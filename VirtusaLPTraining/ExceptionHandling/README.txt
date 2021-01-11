5c7c6b5ab6715d5cccde77b51f93c152f18c697a
Dilshika <dilshika.bandara@gmail.com>
2021-01-11 16:41:41 +0530

    Virtusa LP Training

---
+++ VirtusaLPTraining/ExceptionHandling/README.txt
@@ -0,0 +1,1 @@
+Use: Exception Handling in Java
--- VirtusaLPTraining/ExceptionHandling/src/Task3/AddCourse.java
+++ VirtusaLPTraining/ExceptionHandling/src/Task3/AddCourse.java
@@ -4,15 +4,19 @@ package Task3;
 
 public class AddCourse {
     public static void main(String[] args) {
-
+        try{
+            updateCourse();
+        }catch(InvalidSQLException exception){
+            throw new RuntimeException(exception);  //Wrapped as unchecked Exception
+        }
     }
 
-    private static void updateCourse() {
+    private static void updateCourse() throws InvalidSQLException {
         throw new InvalidSQLException("Invalid sql query");
     }
 
 
-    private static class InvalidSQLException extends RuntimeException{
+    private static class InvalidSQLException extends Exception{
         InvalidSQLException(String message){
             super(message);
         }
